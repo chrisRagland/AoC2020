15285a681413b92035c8133f0c8820a6f6288e72
Chris Ragland <ragland.chris@gmail.com>
2020-12-24 20:54:20 -0900

    Final commit!

--- AoC/Program.cs
+++ AoC/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,6 +11,37 @@ namespace AoC
 	class Program
 	{
 		static void Main(string[] args)
+		{
+			SolveDay25();
+		}
+
+		private static void SolveDay25()
+		{
+			long doorPublicKey = 9232416;
+			long cardPublicKey = 14144084;
+			long divideBy = 20201227;
+			long subjectNumber = 7;
+			long value = 1;
+			int n = 0;
+			while (true)
+			{
+				n++;
+				value *= subjectNumber;
+				value %= divideBy;
+				if (value == cardPublicKey)
+					break;
+			}
+			subjectNumber = doorPublicKey;
+			value = 1;
+			for (int i = 0; i < n; i++)
+			{
+				value *= subjectNumber;
+				value %= divideBy;
+			}
+			Console.WriteLine("Day 25: " + value);
+		}
+
+		private static void SolveDay24()
 		{
 			var input = File.ReadAllLines("Test.txt");
 
@@ -55,49 +87,69 @@ namespace AoC
 					flipped.Add(tileToFlip);
 			}
 
-			int minX = int.MaxValue;
-			int maxX = int.MinValue;
-			int minY = int.MaxValue;
-			int maxY = int.MinValue;
+			Console.WriteLine("Day 24 - Part 1: " + flipped.Count);
 
 			for (int i = 1; i <= 100; i++)
 			{
 				var newFlipped = new HashSet<Tuple<int, int>>(flipped);
+
 				foreach (var item in flipped)
 				{
-					if (item.Item1 > maxX)
-						maxX = item.Item1;
+					
+					var containedCount = 0;
+					if (flipped.Contains(new Tuple<int, int>(item.Item1 - 1, item.Item2)))
+						containedCount++;
 
-					if (item.Item1 < minX)
-						minX = item.Item1;
+					if (flipped.Contains(new Tuple<int, int>(item.Item1, item.Item2 - 1)))
+						containedCount++;
 
-					if (item.Item2 > maxY)
-						maxY = item.Item2;
+					if (flipped.Contains(new Tuple<int, int>(item.Item1 - 1, item.Item2 - 1)))
+						containedCount++;
 
-					if (item.Item2 < minY)
-						minY = item.Item2;
-
-					var nextToTile = new List<Tuple<int, int>>()
+					if (containedCount > 2)
 					{
-						new Tuple<int, int>((item.Item1 - 1), item.Item2),
-						new Tuple<int, int>(item.Item1, (item.Item2 - 1)),
-						new Tuple<int, int>((item.Item1 - 1), (item.Item2 - 1)),
-						new Tuple<int, int>((item.Item1 + 1), item.Item2),
-						new Tuple<int, int>(item.Item1, (item.Item2 + 1)),
-						new Tuple<int, int>((item.Item1 + 1), (item.Item2 + 1)),
-					};
+						newFlipped.Remove(item);
+						continue;
+					}
 
-					int containedCount = nextToTile.Where(x => flipped.Contains(x)).Count();
+					if (flipped.Contains(new Tuple<int, int>(item.Item1 + 1, item.Item2)))
+						containedCount++;
+
+					if (containedCount > 2)
+					{
+						newFlipped.Remove(item);
+						continue;
+					}
+
+					if (flipped.Contains(new Tuple<int, int>(item.Item1, item.Item2 + 1)))
+						containedCount++;
+
+					if (containedCount == 1)
+						continue;
+					else if (containedCount > 2)
+					{
+						newFlipped.Remove(item);
+						continue;
+					}
+
+					if (flipped.Contains(new Tuple<int, int>(item.Item1 + 1, item.Item2 + 1)))
+						containedCount++;
+
 					if (containedCount == 0 || containedCount > 2)
 					{
 						newFlipped.Remove(item);
+						continue;
 					}
+
 				}
 
-				if (i == 1)
-					Console.WriteLine("Day 24 - Part 1: " + flipped.Count);
+				int minX = flipped.Select(x => x.Item1).Min();
+				int maxX = flipped.Select(x => x.Item1).Max();
+				int minY = flipped.Select(x => x.Item2).Min();
+				int maxY = flipped.Select(x => x.Item2).Max();
 
 				var cb = new ConcurrentBag<Tuple<int, int>>();
+
 				Parallel.For((minX - 1), (maxX + 2), x =>
 				{
 					for (int y = (minY - 1); y <= (maxY + 1); y++)
@@ -116,21 +168,20 @@ namespace AoC
 
 						int containedCount = nextToTile.Where(x => flipped.Contains(x)).Count();
 						if (containedCount == 2)
-						{
 							cb.Add(current);
-						}
 					}
 				});
 
 				foreach (var item in cb)
-				{
 					newFlipped.Add(item);
-				}
 
 				flipped = new HashSet<Tuple<int, int>>(newFlipped);
+				//if (i % 25 == 0)
+				//	Console.WriteLine(i + " : " + flipped.Count);
 			}
 
 			Console.WriteLine("Day 24 - Part 2: " + flipped.Count);
+
 		}
 	}
 }
